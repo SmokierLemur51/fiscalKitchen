baf9fae082670561bd0cc59a3bb13d5f793fd7f2
SmokierLemur51 <ldl6147@gmail.com>
2023-04-09 18:16:48 -0400

    some further setup

---
+++ fiscalKitchen/models.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from fiscalKitchen import db, login_manager
+from flask_login import UserMixin
+
+'''
+	Database Tables Needed
+		
+		- User
+		- Items/Ingredients
+			- Separate Table for Kroger API?
+		- Grocery List
+		- Pantry
+		- Recipies
+		- Meals
+		- Posts
+		- Comments
+		- Purchases
+		- Expense Analysis Tracking
+		- Budget
+		- Paycheck
+		- Cashapp/Venmo
+
+'''
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.quey.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), nullable=False, unique=True)
+	email = db.Column(db.String(120), nullable=False, unique=True)
+	password = db.Column(db.String(60), nullable=False)
